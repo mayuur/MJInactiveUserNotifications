9fbbedf252a8530b235b45bdedeb3bb3c2e9c790
Mayur Joshi <mayurjoshi7@gmail.com>
2015-12-23 11:50:32 +0530

    Added code for Local Notification
    - Added sound file
    - Code for registering for Local notifications

--- MJInactiveUserNotifications/AppDelegate.swift
+++ MJInactiveUserNotifications/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: [.Sound, .Alert, .Badge], categories: nil));
+
         return true
     }
 
@@ -27,10 +29,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        UIApplication.sharedApplication().scheduleMJLocalNotification("Max, we miss you! Check out our new updates!", duration: 15, soundName: "notification1.mp3", notificationType: MJNotificationType.MJNotificationTypeInactive);
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
+        application.cancelInactiveMJLocalNotifications();
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
